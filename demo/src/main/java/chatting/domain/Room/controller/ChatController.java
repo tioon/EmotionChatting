1a53c1ec2f4063b97437bf2160493cd908c17707
kimyechan <tioon74@naver.com>
2024-03-28 02:58:07 +0900

    refactor : 채팅 Response DTO 수정

--- demo/src/main/java/chatting/domain/Room/controller/ChatController.java
+++ demo/src/main/java/chatting/domain/Room/controller/ChatController.java
@@ -1,10 +1,9 @@
 package chatting.domain.Room.controller;
 
-import chatting.domain.Room.dto.request.Greeting;
+import chatting.domain.Room.dto.request.ChatResponse;
 import chatting.domain.Room.dto.request.ChatMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
@@ -18,14 +17,16 @@ public class ChatController {
     //@SendTo("/topic/chat") 이건 어노테이션 활용방법
     public void chat(ChatMessage message) throws Exception {
         String roomId = message.getRoomId();
-        messagingTemplate.convertAndSend("/topic/" + roomId, new Greeting(HtmlUtils.htmlEscape(message.getNickname()+ "님 : " + message.getMessage())));
+        messagingTemplate.convertAndSend("/topic/" + roomId,
+                ChatResponse.toResponse(HtmlUtils.htmlEscape(message.getNickname()+ "님 : " + message.getMessage())));
     }
 
     @MessageMapping("/room")
     //@SendTo("/topic/chat") 이건 어노테이션 활용방법
     public void roomEnter(ChatMessage message) throws Exception {
         String roomId = message.getRoomId();
-        messagingTemplate.convertAndSend("/topic/" + roomId,new Greeting(HtmlUtils.htmlEscape(message.getNickname() + "님이 입장하셨습니다.")));
+        messagingTemplate.convertAndSend("/topic/" + roomId,
+                ChatResponse.toResponse(HtmlUtils.htmlEscape(message.getNickname() + "님이 입장하셨습니다.")));
     }
 
 
--- demo/src/main/java/chatting/domain/Room/dto/request/ChatResponse.java
+++ demo/src/main/java/chatting/domain/Room/dto/request/ChatResponse.java
@@ -3,10 +3,16 @@ package chatting.domain.Room.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Greeting {
+public class ChatResponse {
     private String content;
+
+    public static ChatResponse toResponse(String content){
+        return new ChatResponse(content);
+    }
 }
